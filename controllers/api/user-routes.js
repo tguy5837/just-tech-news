ef98f6712879bc567cf210ec4e9ed1ab3b17b8d7
DTGuy <tguy5837@gmail.com>
2022-10-24 21:37:39 -0400

    added login/logout features

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -64,7 +64,15 @@ router.post('/', (req, res) => {
         email: req.body.email,
         password: req.body.password
     })
-        .then(dbUserData => res.json(dbUserData))
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            })
+        })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -90,8 +98,27 @@ router.post('/login', (req, res) => {
             return;
         }
 
-        res.json({ user: dbUserData, message: 'You are now logged in.' })
-    })
+        // save data to session
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({ user: dbUserData, message: 'You are now logged in.' });
+        });
+    });
+});
+
+// logout
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
 })
 
 // PUT /api/users/1
--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require('express');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
+// set up session
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -16,6 +29,9 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// use session
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
